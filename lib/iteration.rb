fbf7529f712e058ddf63fe878eb11f826f04dec5
Zebadiah Blackwell <zeb.blackwell@gmail.com>
2019-11-25 14:33:23 +0000

    Automatically backed up by Learn

--- lib/iteration.rb
+++ lib/iteration.rb
@@ -12,7 +12,7 @@ while row_index < src.count do
   new array << "I love ${src[row_index][0] and ${src[row_index][1] on my pizza"
   row_index += 1
 end
-
+return new_array
 end
 
 def find_greater_pair(src)
