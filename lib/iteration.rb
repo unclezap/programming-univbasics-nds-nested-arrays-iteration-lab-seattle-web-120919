ff24cf1f9708eda4aeaf7ebd50869e0838aa228b
Zebadiah Blackwell <zeb.blackwell@gmail.com>
2019-11-25 14:49:09 +0000

    Automatically backed up by Learn

--- lib/iteration.rb
+++ lib/iteration.rb
@@ -9,12 +9,9 @@ def join_ingredients(src)
 new_array = []
 row_index = 0
 while row_index < src.count do
-  var1 = src[row_index][0]
-  var2 = src[row_index][1]
-  new_array.push("I love " + var1 + " and " + var2 + " on my pizza")
+  new_array.push("I love " + src[row_index][0] + " and " + src[row_index][1] + " on my pizza")
   row_index += 1
 end
-puts new_array
 return new_array
 end
 
