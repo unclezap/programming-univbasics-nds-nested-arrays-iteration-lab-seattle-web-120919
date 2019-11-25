bbd20c559dbc032d76671cfbca867b705073e76f
Zebadiah Blackwell <zeb.blackwell@gmail.com>
2019-11-25 14:45:00 +0000

    Automatically backed up by Learn

--- lib/iteration.rb
+++ lib/iteration.rb
@@ -9,9 +9,9 @@ def join_ingredients(src)
 new_array = []
 row_index = 0
 while row_index < src.count do
-  var1 = 
-  var2 = 
-  new_array << "I love ${var1} and ${var2} on my pizza"
+  var1 = src[row_index][0]
+  var2 = src[row_index][1]
+  new_array.push("I love ${var1} and ${var2} on my pizza")
   row_index += 1
 end
 puts new_array
